e173869a37eea6fbd3d90d60e8c6945014af1ad0
itto <ittokor@gmail.com>
2019-04-22 09:37:45 -0400

    Add sample cleaning script

---
+++ clean_sample.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+print('Reading file...')
+log = pd.read_csv('./data/log_sample.csv',sep='\t')
+
+print('Changing columns names...')
+log.columns = [
+    'ts_id',
+    'key',
+    'input_len',
+    'lev_dist',
+    'autocorr',
+    'predict',
+    'swype',
+    'text_field',
+    'timestamp'    
+]
+
+print('Updating file...')
+log.to_csv('./data/log_sample.csv',index=False)
+      
