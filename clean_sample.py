722f398848f094e1aaffb930eae562faa7cb7e05
itto <ittokor@gmail.com>
2019-08-07 21:45:48 +0200

    Add arguments to clean_sample script

--- clean_sample.py
+++ clean_sample.py
@@ -1,22 +1,28 @@
 import pandas as pd
+import argparse
 
-print('Reading file...')
-log = pd.read_csv('./data/log_sample.csv',sep='\t')
+parser = argparse.ArgumentParser(description='Clean log data')
+parser.add_argument('--path',required=True,help='Path to the log data to be cleaned')
+args = parser.parse_args()
 
-print('Changing columns names...')
-log.columns = [
-    'ts_id',
-    'key',
-    'input_len',
-    'lev_dist',
-    'autocorr',
-    'predict',
-    'swype',
-    'text_field',
-    'timestamp'    
-]
+if __name__ == "__main__":
+    print('Reading file...')
+    log = pd.read_csv(args.path,sep='\t')
 
-print('Updating file...')
-log.to_csv('./data/log_sample.csv',index=False)
-print('DONE')
+    print('Changing columns names...')
+    log.columns = [
+        'ts_id',
+        'key',
+        'input_len',
+        'lev_dist',
+        'autocorr',
+        'predict',
+        'swype',
+        'text_field',
+        'timestamp'    
+    ]
+
+    print('Updating file...')
+    log.to_csv(args.path,index=False)
+    print('DONE')
       
